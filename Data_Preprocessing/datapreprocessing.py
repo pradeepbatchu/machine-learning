c03f9b4e8f4b13f37f36f91a75f085e958cc0657
Pradeep <b.pradeep.p@gmail.com>
2018-09-23 13:27:25 +0530

    Update datapreprocessing.py

--- Data_Preprocessing/datapreprocessing.py
+++ Data_Preprocessing/datapreprocessing.py
@@ -7,25 +7,25 @@ import random
 
 #dataset
 data=pd.read_csv("D:/DS/Data.csv",encoding='utf-8')
-x=data.iloc[:,0:-1].values
+X=data.iloc[:,0:-1].values
 y=data.iloc[:, 3].values
 
 
 ###taking care of missing data
 from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values="NaN", strategy="mean", axis=0)
-imputer = imputer.fit(x[:, 1:3])
-x[:, 1:3]=imputer.transform(x[:, 1:3])
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3]=imputer.transform(X[:, 1:3])
 
 ##Encoding categorical data Independent variable
 ##Dummy Data
 
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-labelEncoder_x= LabelEncoder()
-x[:,0] = labelEncoder_x.fit_transform(x[:,0])
+labelEncoder_X= LabelEncoder()
+X[:,0] = labelEncoder_X.fit_transform(X[:,0])
 
 onehotencoder = OneHotEncoder(categorical_features= [0])
-x = onehotencoder.fit_transform(x).toarray()
+X = onehotencoder.fit_transform(X).toarray()
 
 ##Encoding Dependent variable
 ## In Genral we will not do onehotencoder on Dependent variables
@@ -37,14 +37,14 @@ y= labelEncoder_y.fit_transform(y)
 #### Splitting the data set into Training set and Test Set
 from sklearn.cross_validation import train_test_split
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size= 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.2, random_state = 0)
 
 ####Scaling##
 
 from sklearn.preprocessing import StandardScaler 
-sc_x= StandardScaler()
-x_train = sc_x.fit_transform(x_train)
-x_test = sc_x.transform(x_test)
+sc_X= StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
 
 
 #### Final data preprocessing###
